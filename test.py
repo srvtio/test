80f8bd0ce71162274c8b73f31245f9d1b706834f
Fumiyoshi Kasahara <SymFrhsk@FKMac.local>
2020-09-06 20:10:24 +0900

    add2line

--- test.py
+++ test.py
@@ -4,5 +4,7 @@ data = pd.DataFrame()
 data["id"] = [0, 1, 2]
 data["name"] = ['upp', 'ueu', 'otaga']
 data["kd"] = [0.8, 0.9, 1.4]
+data["kd2"] = [0.8, 0.9, 1.4]
+data["kd3"] = [0.8, 0.9, 1.4]
 
 print(data)
